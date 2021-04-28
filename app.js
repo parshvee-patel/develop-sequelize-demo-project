3bb3422a66f12c6eb609478a29380a218669b0fd
parshvee_patel <patel.pari3097@gmail.com>
2021-04-28 17:06:15 +0530

    Add cors

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 import express from "express";
-import bodyparser from "body-parser"
+import bodyparser from "body-parser";
+import cors from "cors"
 
 //Local Import Files
 import * as config from "./config";
 import { routerV1 } from "./routes";
 
 const app = express();
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use("/api/v1", routerV1);
 
 // simple route
-app.get("/", function(req, res) {
-    res.send("Hello World!");
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
 // set port, listen for requests
 app.listen(config.PORT, () => {
-    console.log("Staff app listening on" + " " + config.PORT);
+  console.log("Staff app listening on" + " " + config.PORT);
 });
